8f2c1de53373c9948a93da28980aea6642dd4c6f
Flavius <1195165+toriqo@users.noreply.github.com>
2025-02-07 12:29:20 +0200

    small fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,27 @@
-import { useState, useCallback } from 'react'
-import './App.css'
-import { Input } from './components/Input'
-import { Button } from './components/Button'
+import { useState, useCallback } from 'react';
+import './App.css';
+import { Input } from './components/Input';
+import { Button } from './components/Button';
 
 // TODO: implement the signin form
 function App() {
-  const [email, setEmail] = useState('')
-  const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  }, []);
+  const [email, setEmail] = useState('');
 
-  const customAlert = useCallback(() => {
+  const handleChange = (e: any) => {
+    setEmail(e.target.value);
+  };
+
+  const submitForm = useCallback(() => {
     alert(email);
-  }, []);
+  }, [email]);
 
   return (
-    <div className='App'>
+    <div className="App">
       <h2>Signin Form</h2>
       <Input placeholder={'john@doe.com'} value={email} onChange={handleChange}></Input>
-      <Button onClick={customAlert}>Signin</Button>
+      <Button onClick={submitForm}>Signin</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
